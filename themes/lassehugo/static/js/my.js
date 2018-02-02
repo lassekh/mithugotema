7e8a4fbc667436078a9551ea170d7ff9d31b89f9
lassekh <lasse.kh.92@gmail.com>
2018-02-02 10:34:28 +0100

    New Menu

--- themes/lassehugo/static/js/my.js
+++ themes/lassehugo/static/js/my.js
@@ -1,9 +1,9 @@
-/* Set the width of the side navigation to 250px */
+/* Open when someone clicks on the span element */
 function openNav() {
-    document.getElementById("mySidenav").style.width = "250px";
+    document.getElementById("myNav").style.width = "100%";
 }
 
-/* Set the width of the side navigation to 0 */
+/* Close when someone clicks on the "x" symbol inside the overlay */
 function closeNav() {
-    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("myNav").style.width = "0%";
 }
